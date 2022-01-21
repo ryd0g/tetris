c2ee1a4142b0ef9d4197206148118f3b4e0985ac
Ryan <rjlguitar@gmail.com>
2022-01-20 20:53:58 -0800

    added store, provider, utils

--- tetris-app/src/App.js
+++ tetris-app/src/App.js
@@ -1,22 +1,30 @@
 import "./App.css";
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 import GridBoard from "./components/gridboard";
 import NextBlock from "./components/nextblock";
 import ScoreBoard from "./components/scoreboard";
 import Controls from "./components/controls";
 import MessagePopup from "./components/messagepopup";
 
+const store = createStore(reducers);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Tetris Redux</h1>
-      </header>
-      <GridBoard />
-      <NextBlock />
-      <ScoreBoard />
-      <Controls />
-      <MessagePopup />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Tetris Redux</h1>
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+        <MessagePopup />
+      </div>
+    </Provider>
   );
 }
 
