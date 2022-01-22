48106584732ab8fe33ad5a0f20d1cb2b7e511cce
Ryan <rjlguitar@gmail.com>
2022-01-21 17:59:52 -0800

    added provider

--- tetris-app/src/App.js
+++ tetris-app/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
 import reducers from "./reducers";
 import GridBoard from "./components/gridboard";
 import NextBlock from "./components/nextblock";
