37236470b5cc66d6bb37119527010df50b5af3b9
Ryan <rjlguitar@gmail.com>
2022-01-21 20:04:29 -0800

    added moving blocks, pause, resume

--- tetris-app/src/components/gridboard.js
+++ tetris-app/src/components/gridboard.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import GridSquare from "./gridsquare";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { shapes } from "../utils";
+import { moveDown } from "../actions";
 
 export default function GridBoard(props) {
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+  const dispatch = useDispatch();
   const game = useSelector((state) => state.game);
   const { grid, shape, rotation, x, y, isRunning, speed } = game;
   const block = shapes[shape][rotation];
   const blockColor = shape;
   // map rows
+
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+    lastUpdateTimeRef.current = time;
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
   const gridSquares = grid.map((rowArray, row) => {
     // map columns
     return rowArray.map((square, col) => {
